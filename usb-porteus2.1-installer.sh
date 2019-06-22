43634b9d0f848e6dbea73a40be72bb1459826b9f
Wenceslao <wenceslaomateos@gmail.com>
2019-06-22 17:45:06 -0300

    Cambios menores

--- usb-porteus2.1-installer.sh
+++ usb-porteus2.1-installer.sh
@@ -5,14 +5,15 @@
 
 # 2. Al utilizarlo en una PC con una configuración de mas de un HDD, el mismo tiende a fallar.
 
-# 3. Al ser un script de BASH, el mismo solo corre sobre Linux, por lo que se podria crear 
-#   un equivalente en PowerShell para su portabilidad.
-
-# 4. Demás errores que comenten en el campus virtual.
+# 3. Si el usuario al ejecutar el instalador por algun motivo interrumpe el programa, el mismo al volver 
+#   a ser ejecutado no funciona.
 
 
-#Luego de la primera prueba, en las dos condiciones de error pasadas, el equipo funciona correctamente
-#pero el usb no es reconocido por mi computadora, y en la de martin tira un error
+# Luego de la primera prueba, en las dos condiciones de error pasadas, el equipo funciona correctamente
+# pero el usb no es reconocido por mi computadora, y en la de martin tira un error
+
+# Volvimos a probarlo pero esta vez ejecutamos todo desde el home, anduvo perfectamente con la salvedad
+# de que no anda la persistencia.
 
 #----------------------------------------------------------------------#
 #                        SCRIPT DE BASH
@@ -115,11 +116,9 @@ w
 EOF2
 
 # actualiza tabla de particiones del sistema
-
 partprobe
 
 # formatea ambas particiones, en el FileSystem correspondiente
-
 par1="$device"1
 par2="$device"2
 
@@ -130,6 +129,7 @@ EOF
 mkfs.vfat -F 32 $par1
 
 echo -e "\\033[1;33mCreando directorios...\\033[0m"
+
 # crea directorio para montar la particion FAT32
 mkdir /mnt/usbFat
 # crea directorio para montar la particion EXT4
@@ -138,6 +138,7 @@ mkdir /mnt/usbExt
 mkdir /mnt/iso
 
 echo -e "\\033[1;33mMontando archivos y particiones...\\033[0m"
+
 # monta imagen ISO
 #aca podriamos agregar un wget para descargar la iso y no tener que andar paseandola
 mount -o loop AN_PORTEUS2.1_x86_64.iso /mnt/iso
